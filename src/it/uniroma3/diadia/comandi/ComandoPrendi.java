719c08254417c8d17cf6032fc32a2ee0ce0bc5d6
Davideee03 <166947518+Davideee03@users.noreply.github.com>
2024-04-25 11:07:36 +0200

    Add files via upload

---
+++ src/it/uniroma3/diadia/comandi/ComandoPrendi.java
@@ -0,0 +1,25 @@
+package it.uniroma3.diadia.comandi;
+
+import it.uniroma3.diadia.Partita;
+import it.uniroma3.diadia.attrezzi.Attrezzo;
+
+public class ComandoPrendi implements Comando{
+	
+	private String attrezzo;
+	
+	@Override 
+	public void esegui(Partita partita) {
+		Attrezzo att = partita.getLabirinto().getStanzaCorrente().getAttrezzo(attrezzo);
+		partita.getGiocatore().getBorsa().addAttrezzo(att);
+		partita.getLabirinto().getStanzaCorrente().removeAttrezzo(att);
+	}
+	
+	@Override
+	public void setParametro(String parametro) {
+		this.attrezzo = parametro;
+	}
+	
+	public String getAttrezzo() {
+		return this.attrezzo;
+	}
+}
