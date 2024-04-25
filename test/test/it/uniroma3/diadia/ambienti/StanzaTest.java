3b735ec82145d5f0bbad9badd18f135057fc3094
Davideee03 <166947518+Davideee03@users.noreply.github.com>
2024-04-25 11:10:46 +0200

    Add files via upload

---
+++ test/test/it/uniroma3/diadia/ambienti/StanzaTest.java
@@ -0,0 +1,42 @@
+package it.uniroma3.diadia.ambienti;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+
+import org.junit.Test;
+
+import it.uniroma3.diadia.attrezzi.Attrezzo;
+
+public class StanzaTest {
+	private Stanza stanzina;
+	private Stanza stanzona;
+	private Attrezzo attrezzo;
+
+	
+	
+	@Before
+	public void setUp() {
+		this.stanzina = new Stanza("vuota");
+		this.stanzona = new Stanza("nonVuota");
+		this.attrezzo = new Attrezzo("spada", 10);
+		stanzona.addAttrezzo(attrezzo);
+	}
+	
+	@Test
+	public void testStanzaSenzaAttrezzi() {	
+		assertNull(stanzina.getAttrezzo("NonCiSonoeheh"));
+	}
+	
+	@Test
+	public void testStanzaNonVuota() {	
+		assertNotNull(stanzona.getAttrezzo("spada"));
+	}
+	
+	@Test
+	public void testStanzaNonVuotaAttrezzoAssente() {	
+		assertNull(stanzona.getAttrezzo("martello"));
+	}
+	
+
+}
