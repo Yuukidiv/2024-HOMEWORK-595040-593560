4802563cce81bad67527b4d320d84dc2fa96d68a
Yuuki <67429515+Yuukidiv@users.noreply.github.com>
2024-06-06 13:35:27 +0200

    Add files via upload

---
+++ test/it/uniroma3/diadia/giocatore/GiocatoreTest.java
@@ -0,0 +1,37 @@
+package it.uniroma3.diadia.giocatore;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class GiocatoreTest {
+
+	private Giocatore giocatore;
+	private Giocatore giocatore2;
+
+	
+	@Before
+	public void setUp() {
+		this.giocatore= new Giocatore();
+		this.giocatore2 = new Giocatore();
+		this.giocatore2.setCfu(100);
+
+	}
+	
+	@Test
+	public void testCfuInizialiGiusti() {
+		assertEquals(20, giocatore.getCfu());
+	}
+
+	@Test
+	public void testCfuCambiati() {
+		assertEquals(100, giocatore2.getCfu());
+	}
+
+	@Test
+	public void testBorsaPresente() {
+		assertNotNull( giocatore.getBorsa());
+	}
+
+}
